f637e8733c2588cb51d379128ae9d86adf321337
Kelvin Tan <superkelvint@gmail.com>
2025-10-24 18:29:00 -0700

    Fix wrong escaping of unicode.

--- tests/test_javascriptencoder.nim
+++ tests/test_javascriptencoder.nim
@@ -32,8 +32,8 @@ suite "JavaScriptEncoder Tests":
           builder = builder.encode("\\\"", "\"")
           builder = builder.encode("\\'", "'")
         of HTML, ATTRIBUTE:
-          builder = builder.encode("\\\\x22", "\"")
-          builder = builder.encode("\\\\x27", "'")
+          builder = builder.encode("\\x22", "\"")
+          builder = builder.encode("\\x27", "'")
 
         case mode
         of BLOCK, HTML:
@@ -50,12 +50,12 @@ suite "JavaScriptEncoder Tests":
         builder = builder.encodeWithName("backspace", "\\b", "\b")
         builder = builder.encodeWithName("tab", "\\t", "\t")
         builder = builder.encodeWithName("LF", "\\n", "\n")
-        builder = builder.encodeWithName("vtab", "\\\\x0b", cp(0x0b))
+        builder = builder.encodeWithName("vtab", "\\x0b", cp(0x0b))
         builder = builder.encodeWithName("FF", "\\f", "\f")
         builder = builder.encodeWithName("CR", "\\r", "\r")
-        builder = builder.encodeWithName("NUL", "\\\\x00", cp(0))
-        builder = builder.encodeWithName("Line Separator", "\\\\u2028", "\u2028")
-        builder = builder.encodeWithName("Paragraph Separator", "\\\\u2029", "\u2029")
+        builder = builder.encodeWithName("NUL", "\\x00", cp(0))
+        builder = builder.encodeWithName("Line Separator", "\\u2028", "\u2028")
+        builder = builder.encodeWithName("Paragraph Separator", "\\u2029", "\u2029")
         builder = builder.encode("abc", "abc")
         builder = builder.encode("ABC", "ABC")
 
@@ -70,8 +70,8 @@ suite "JavaScriptEncoder Tests":
 
           builder = builder.encoded("\u2028\u2029")
         else:
-          builder = builder.encodeWithName("unicode", "\\\\u1234", "\u1234")
-          builder = builder.encodeWithName("high-ascii", "\\\\xff", "\u00ff")
+          builder = builder.encodeWithName("unicode", "\\u1234", "\u1234")
+          builder = builder.encodeWithName("high-ascii", "\\xff", "\u00ff")
           builder = builder.encoded(0x7f, MaxCodePoint)
         
         # Run suites
